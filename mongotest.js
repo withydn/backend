7a53f164411d60e462d8ae6864a13e74404cb121
withydn <withydn@naver.com>
2022-12-04 14:01:31 +0900

    ok

--- mongotest.js
+++ mongotest.js
@@ -1,7 +1,7 @@
 // @ts-check
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = "mongodb+srv://areum:ahdtlf!237@cluster0.uf6iqgu.mongodb.net/?retryWrites=true&w=majority";
+const uri = 'mongodb+srv://areum:ahdtlf!237@cluster0.uf6iqgu.mongodb.net/?retryWrites=true&w=majority';
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
