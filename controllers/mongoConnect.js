8609f6a442e5ed8614266fdb60851fbd87ccb6d1
withydn <withydn@naver.com>
2022-12-04 13:26:07 +0900

    ok

--- controllers/mongoConnect.js
+++ controllers/mongoConnect.js
@@ -3,7 +3,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri =
-  'mongodb+srv://areum:ahdtlf!237@cluster0.sdiakr0.mongodb.net/?retryWrites=true&w=majority';
+'mongodb+srv://areum:ahdtlf!237@cluster0.uf6iqgu.mongodb.net/?retryWrites=true&w=majority';
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
